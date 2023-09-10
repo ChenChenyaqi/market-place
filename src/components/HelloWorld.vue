543f258fd5e2c42da8401a2615349dcb00580ff5
chenyaqi <yaqi.chen@definesys.com>
2023-09-10 10:15:24 +0800

    feat: 引入tailwind

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,6 +3,7 @@
     <el-button type="primary" size="default" @click="handleClick"
       >click me!</el-button
     >
+    <div class="bg-red-200">hello world!</div>
   </div>
 </template>
 
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
 import App from './App.vue'
 import pinia from './store'
 import router from './router'
+import './style.less'
+import 'element-plus/dist/index.css'
 
 const app = createApp(App)
 app.use(pinia)
